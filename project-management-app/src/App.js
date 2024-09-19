8941ea8dfa2b7748a79495a7417018db19c19c22
mmladjani <mmiljus24@gmail.com>
2024-09-19 22:34:29 +0200

    update to useContext hook

--- project-management-app/src/App.js
+++ project-management-app/src/App.js
@@ -2,11 +2,11 @@ import Sidebar from './components/main/Sidebar';
 import MainContent from './components/main/MainContent';
 import { useState, useContext } from 'react';
 import { ProjectContext } from './store/ProjectContext';
+import ProjectContextProvider from './store/ProjectContext';
 
 function App() {
 
   const { items } = useContext(ProjectContext);
-  console.log(items);
 
   const [formVisible, setFormVisible] = useState(false);
   const [projectList, setProjectList] = useState('');
@@ -18,23 +18,25 @@ function App() {
   }
 
   return (
-    <div className="flex my-9 flex-row h-screen">
-      <Sidebar
-          setFormVisible={setFormVisible}
-          setSelectedProject={setSelectedProject}
-          projectList={projectList || []}
-          openForm={openForm} 
-        />
-      <MainContent 
-          setFormVisible={setFormVisible}
-          formVisible={formVisible}
-          setProjectList={setProjectList}
-          projectList={projectList}
-          setSelectedProject={setSelectedProject}
-          selectedProject={selectedProject}
-          openForm={openForm}
-        />
-    </div>
+    <ProjectContextProvider>
+      <div className="flex my-9 flex-row h-screen">
+        <Sidebar
+            setFormVisible={setFormVisible}
+            setSelectedProject={setSelectedProject}
+            projectList={projectList || []}
+            openForm={openForm} 
+          />
+        <MainContent 
+            setFormVisible={setFormVisible}
+            formVisible={formVisible}
+            setProjectList={setProjectList}
+            projectList={projectList}
+            setSelectedProject={setSelectedProject}
+            selectedProject={selectedProject}
+            openForm={openForm}
+          />
+      </div>
+    </ProjectContextProvider>
   )
 }
 
--- project-management-app/src/store/ProjectContext.js
+++ project-management-app/src/store/ProjectContext.js
@@ -1,5 +1,22 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 export const ProjectContext = createContext({
-    items: []
+    projectList: []
 });
+
+function projectListReducer(state, action){
+    console.log('test');
+}
+
+export default function ProjectContextProvider({children}){
+
+    const [projectListState, projectListDispatch] = useReducer(projectListReducer, {
+        projectList: []
+    })
+
+    return (
+        <ProjectContext.Provider value={''}>
+            {children}
+        </ProjectContext.Provider>
+    )
+}
